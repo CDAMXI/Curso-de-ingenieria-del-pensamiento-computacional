ec36182ad685633506662cd7f7d5cbf5271e9fb2
CDAMXI <cdachongm@gmail.com>
2025-04-19 12:01:48 +0200

    Add Fibonacci recursive implementation in Java

---
+++ Nivel 2/fibonacciIterativo.java
@@ -0,0 +1,13 @@
+public class fibonacciIterativo {
+    public static void main(String[] args) {
+        int n = 10; // Cambia este valor para calcular otros números de Fibonacci
+        System.out.println("El número Fibonacci en la posición " + n + " es: " + fibonacci(n));
+    }
+
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
