664525bde86220fa57782e752ef2b818916de249
CDAMXI <cdachongm@gmail.com>
2025-04-19 12:06:59 +0200

    Add detailed comments explaining function results in EjemploPráctico.java

--- Nivel_2/EjemploPráctico.java
+++ Nivel_2/EjemploPráctico.java
@@ -6,5 +6,12 @@ public class EjemploPráctico {
         double fun2 = (3 + 4) / 2;
         System.out.println("Resultado de la función 1 (Sin paréntesis): " + fun1);
         System.out.println("Resultado de la función 2 (Con paréntesis): " + fun2);
-    }
+
+        /*
+         * El resultado de la función 1 es 5.0 y el de la función 2 es 3.5
+         * El motivo de porque la función 1 sale como 5.0 es por el tipo de variables que se están utilizando,
+         * ya que al ser enteros, la división se hace entre enteros y no entre decimales. Por lo tanto, el 
+         * resultado de 4/2 es 2 y luego se suma a 3, dando como resultado 5.0.
+         */
+        }
 }
