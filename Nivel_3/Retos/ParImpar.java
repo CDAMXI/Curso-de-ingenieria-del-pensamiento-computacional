b66e40a6611bd9f6d65d5add63b2f779606896a1
CDAMXI <cdachongm@gmail.com>
2025-07-09 12:34:57 +0200

    Refactor ParImpar.java to use try-with-resources for Scanner initialization

--- Nivel_3/Retos/ParImpar.java
+++ Nivel_3/Retos/ParImpar.java
@@ -2,21 +2,21 @@ import java.util.*;
 
 public class ParImpar{
     public static void main(String[] args) {
-        Scanner kbd = new Scanner(System.in);
-        int valor = 0;
-        while(valor < 0){
-            System.out.print("Introduce un valor: ");
-            valor = kbd.nextInt();
-            if(valor < 0){
-                System.out.println("El valor es negativo. Finalizando programa.");
-                return;
-            }
-            else if(valor % 2 == 0){
-                System.out.println("El valor " + valor + " es par.");
-            } else {
-                System.out.println("El valor " + valor + " es impar.");
+        try (Scanner kbd = new Scanner(System.in)) {
+            int valor = 0;
+            while(valor < 0){
+                System.out.print("Introduce un valor: ");
+                valor = kbd.nextInt();
+                if(valor < 0){
+                    System.out.println("El valor es negativo. Finalizando programa.");
+                    return;
+                }
+                else if(valor % 2 == 0){
+                    System.out.println("El valor " + valor + " es par.");
+                } else {
+                    System.out.println("El valor " + valor + " es impar.");
+                }
             }
         }
-        kbd.close();
     }
 }
