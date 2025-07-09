3638d45e1d3f924fff49322a226c5bbaf1f8e11a
CDAMXI <cdachongm@gmail.com>
2025-07-09 12:19:55 +0200

    Add ParImpar program implementations in Java and Python for even/odd value checking

---
+++ Nivel_3/Retos/ParImpar.java
@@ -0,0 +1,19 @@
+public class ParImpar{
+    public static void main(String[] args) {
+        Scanner kbd = new Scanner(System.in);
+        int valor = 0;
+        while(valor < 0){
+            System.out.print("Introduce un valor: ");
+            valor = kbd.nextInt();
+            if(valor < 0){
+                System.out.println("El valor es negativo. Finalizando programa.");
+                return;
+            }
+            else if(valor % 2 == 0){
+                System.out.println("El valor " + valor + " es par.");
+            } else {
+                System.out.println("El valor " + valor + " es impar.");
+            }
+        }
+    }
+}
---
+++ Nivel_3/Retos/ParImpar.py
@@ -0,0 +1,4 @@
+n = int(input("Introduce un valor: "))
+if n < 0: print("El valor es negativo. Finalizando programa.")
+elif(n mod 2 == 0): print("El valor " + valor + " es par.")
+else print("El valor " + valor + "es impar.")
