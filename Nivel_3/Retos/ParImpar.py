ff259c3ed7003a33c5059f4fb7449ff3a905834a
CDAMXI <cdachongm@gmail.com>
2025-07-09 12:32:21 +0200

    Refactor ParImpar function to improve structure and readability

--- Nivel_3/Retos/ParImpar.py
+++ Nivel_3/Retos/ParImpar.py
@@ -1,6 +1,10 @@
-def ParImpar(n): 
-    while(True): 
-        if n < 0: break 
-        elif n % 2 == 0: print(f"El valor {n} es par.")
-        else: print(f"El valor {n} es impar.")
-ParImpar(int(input("Introduce un número: ")))
+def ParImpar(n):
+    if n % 2 == 0: print (f"El valor {n} es par.")
+    else:  print(f"El valor {n} es impar.") 
+        
+while True:
+    n = int(input("Ingrese un valor: "))
+    if n < 0:
+        print("El valor es negativo. Finalizando programa.")
+        break
+    ParImpar(n)
